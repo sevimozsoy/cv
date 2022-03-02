cda266384efd2fe7303fd81a9393c9ae8054bdba
sevimozsoy <sevimozsoy30@gmail.com>
2022-03-02 20:52:33 +0300

    update on contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,7 +102,7 @@ let resumeButton = document.getElementById('resume-button')
 // Html2pdf options
 let opt = {
     margin: 0,
-    filename: 'SevimSelinOZSOY_resume.pdf',
+    filename: 'SevimSelinOZSOY.pdf',
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 4 },
     jsPDF: { format: 'a4', orientation: 'portrait' }
